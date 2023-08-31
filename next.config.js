76c663045ea294a7207d4b68a97fc40cd1555b6c
Girik <68961075+Gyryk@users.noreply.github.com>
2023-09-01 00:06:41 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const plugins = require('next-compose-plugins')
-const images = require('next-optimized-images')
-const withImages = require("next-images");
-
-module.exports = plugins([images, withImages], {source: 'pages', output: 'standalone', images: {
-    disableStaticImages: true,
-    },} )
